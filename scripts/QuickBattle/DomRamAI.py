e676bb9af3bb680425adb7ffa5338279c2df1468
Jagard11 <Justinrs@gmail.com>
2022-09-01 13:49:51 -0600

    Added many changes to DomRamAI

--- scripts/QuickBattle/DomRamAI.py
+++ scripts/QuickBattle/DomRamAI.py
@@ -14,7 +14,79 @@ def CreateAI(pShip, pFriendlies=MissionLib.GetFriendlyGroup()):
 	# Done creating CompoundAI Attack
 	#########################################
 	#########################################
-	# Creating PreprocessingAI AvoidObstacles at (126, 263)
+	# Creating PlainAI Turn at (237, 47)
+	pTurn = App.PlainAI_Create(pShip, "Turn")
+	pTurn.SetScriptModule("ManeuverLoop")
+	pTurn.SetInterruptable(1)
+	pScript = pTurn.GetScriptInstance()
+	pScript.SetLoopFraction(0.25)
+	pScript.SetTurnAxis(App.TGPoint3_GetModelLeft())
+	# Done creating PlainAI Turn
+	#########################################
+	#########################################
+	# Creating PlainAI Turn_2 at (353, 55)
+	pTurn_2 = App.PlainAI_Create(pShip, "Turn_2")
+	pTurn_2.SetScriptModule("ManeuverLoop")
+	pTurn_2.SetInterruptable(1)
+	pScript = pTurn_2.GetScriptInstance()
+	pScript.SetLoopFraction(0.25)
+	pScript.SetTurnAxis(App.TGPoint3_GetModelRight())
+	# Done creating PlainAI Turn_2
+	#########################################
+	#########################################
+	# Creating PlainAI Turn_3 at (429, 103)
+	pTurn_3 = App.PlainAI_Create(pShip, "Turn_3")
+	pTurn_3.SetScriptModule("ManeuverLoop")
+	pTurn_3.SetInterruptable(1)
+	pScript = pTurn_3.GetScriptInstance()
+	pScript.SetLoopFraction(0.25)
+	pScript.SetTurnAxis(App.TGPoint3_GetModelUp())
+	# Done creating PlainAI Turn_3
+	#########################################
+	#########################################
+	# Creating PlainAI Turn_4 at (448, 147)
+	pTurn_4 = App.PlainAI_Create(pShip, "Turn_4")
+	pTurn_4.SetScriptModule("ManeuverLoop")
+	pTurn_4.SetInterruptable(1)
+	pScript = pTurn_4.GetScriptInstance()
+	pScript.SetLoopFraction(0.25)
+	pScript.SetTurnAxis(App.TGPoint3_GetModelDown())
+	# Done creating PlainAI Turn_4
+	#########################################
+	#########################################
+	# Creating RandomAI FlyPointlessly at (198, 181)
+	pFlyPointlessly = App.RandomAI_Create(pShip, "FlyPointlessly")
+	pFlyPointlessly.SetInterruptable(1)
+	# SeqBlock is at (309, 185)
+	pFlyPointlessly.AddAI(pTurn)
+	pFlyPointlessly.AddAI(pTurn_2)
+	pFlyPointlessly.AddAI(pTurn_3)
+	pFlyPointlessly.AddAI(pTurn_4)
+	# Done creating RandomAI FlyPointlessly
+	#########################################
+	#########################################
+	# Creating SequenceAI RepeatForever at (195, 224)
+	pRepeatForever = App.SequenceAI_Create(pShip, "RepeatForever")
+	pRepeatForever.SetInterruptable(1)
+	pRepeatForever.SetLoopCount(-1)
+	pRepeatForever.SetResetIfInterrupted(1)
+	pRepeatForever.SetDoubleCheckAllDone(1)
+	pRepeatForever.SetSkipDormant(0)
+	# SeqBlock is at (295, 228)
+	pRepeatForever.AddAI(pFlyPointlessly)
+	# Done creating SequenceAI RepeatForever
+	#########################################
+	#########################################
+	# Creating PriorityListAI PriorityList at (30, 228)
+	pPriorityList = App.PriorityListAI_Create(pShip, "PriorityList")
+	pPriorityList.SetInterruptable(1)
+	# SeqBlock is at (149, 235)
+	pPriorityList.AddAI(pAttack, 1)
+	pPriorityList.AddAI(pRepeatForever, 2)
+	# Done creating PriorityListAI PriorityList
+	#########################################
+	#########################################
+	# Creating PreprocessingAI AvoidObstacles at (29, 285)
 	## Setup:
 	import AI.CustomPreprocessors
 	pScript = AI.CustomPreprocessors.AvoidObstaclesRam()
@@ -22,7 +94,7 @@ def CreateAI(pShip, pFriendlies=MissionLib.GetFriendlyGroup()):
 	pAvoidObstacles = App.PreprocessingAI_Create(pShip, "AvoidObstacles")
 	pAvoidObstacles.SetInterruptable(1)
 	pAvoidObstacles.SetPreprocessingMethod(pScript, "Update")
-	pAvoidObstacles.SetContainedAI(pAttack)
+	pAvoidObstacles.SetContainedAI(pPriorityList)
 	# Done creating PreprocessingAI AvoidObstacles
 	#########################################
 	#########################################
